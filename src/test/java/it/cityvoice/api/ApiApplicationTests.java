421a67b2dfbafec73c05456d69dc03052f4e462c
AndreaDevelope <andrea_develope@outlook.it>
2026-08-09 17:58:26 +0200

    first commit

---
+++ src/test/java/it/cityvoice/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cityvoice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
